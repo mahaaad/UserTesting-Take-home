f0d4e2697834b72e50515cf7bd048d0c38ff7a49
mahaaad <33506722+mahaaad@users.noreply.github.com>
2022-05-08 02:31:58 -0400

    track component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import './App.css';
+import Track from "./components/Track"
 
 function App() {
   return (
     <div className="App">
+      <nav>
 
+      </nav>
+
+      <div className="playlist-vitals">
+
+      </div>
+
+      <div className="playlist-toolbar">
+
+      </div>
+
+      <div className="playlist-header">
+
+      </div>
+
+      <hr />
+
+      <ol className="tracks-section">
+        <Track
+          albumCover = "./img/Album Artwork.png"
+          songName = "Space Ghoast Coast to Coast"
+          artist = "Glass Animals"
+          albumName = "Dreamland (+ Bonus Levels)"
+          dateAdded = "2 months ago"
+          duration = "3:07"
+        />
+      </ol>
     </div>
   );
 }
--- src/components/Track.js
+++ src/components/Track.js
@@ -2,8 +2,28 @@ import React from "react";
 
 function Track(props){
     return(
-        <div>
-            
-        </div>
+        <li className="track">
+            <img className="track-album-cover" src={props.albumCover}/>
+            {/*Song name and artist*/}
+            <div className="track-name-and-artist">
+                <p>{props.songName}</p>
+                <p>{props.artist}</p>
+            </div>
+
+            {/*Album name*/}
+            <span className="track-album-name">{props.albumName}</span>
+
+            {/*Date added*/}
+            <span className="track-date-added">{props.dateAdded}</span>
+
+            {/*Like button*/}
+            <button></button>
+
+            {/*Duration*/}
+            <span className="track-duration">{props.duration}</span>
+
+        </li>
     )
 }
+
+export default Track;
