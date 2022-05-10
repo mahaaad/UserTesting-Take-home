895cf063fe166a646ddc078c44a8693dee4e800c
mahaaad <33506722+mahaaad@users.noreply.github.com>
2022-05-10 14:25:49 -0400

    added dropdown menu

---
+++ src/components/DropDownMenu.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function DropDownMenu(props){
+    let listItems = []
+    console.log("MENU: " + props.height)
+    for (let i = 0; i < props.items.length; i++){
+        let classes = "menu-item"
+        if (typeof props.items[i] === typeof (<hr />)){
+            classes += " menu-divider"
+        }
+        listItems.push(<li className={classes}>{props.items[i]}</li>)
+    }
+    return(
+        <ul className="drop-down-menu" style={{maxHeight: props.height, opacity: props.opacity}}>
+            {listItems.map((item) => {
+                return item
+            })}
+        </ul>
+    )
+}
+
+export default DropDownMenu
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
+import DropDownMenu from "./DropDownMenu";
 
 function Nav(){
+    const MenuItems = ["Account", "Profile", "Private Session", "Settings", <hr />, "Log Out"]
+    const [menuHeight, setMenuHeight] = useState("0")
+    const [menuOpacity, setMenuOpacity] = useState("0")
+
+    function handleMenuClick(){
+        if (menuHeight === "0"){
+            setMenuHeight("500px")
+        }
+        else{
+            setMenuHeight("0")
+        }
+        if (menuOpacity === "0"){
+            setMenuOpacity("1")
+        }
+        else{
+            setMenuOpacity("0")
+        }
+    }
     return(
         <nav>
             <div id='nav-buttons'>
@@ -12,11 +31,17 @@ function Nav(){
                 </button>
             </div>
             <img src='./img/MahadRehan.jpg' className="icon" id="mobile-pfp" />
-            <button id="account-dropdown" className='button'>
+            <button id="account-drop-down" className='button' onClick={handleMenuClick}>
                 <img src='./img/MahadRehan.jpg' className="icon" style={{width: "1.5rem"}}/>
                 mahadrehan
-                <img src='./img/icons/down.png'/>  
+                <img src='./img/icons/down.png'/>
+                <DropDownMenu
+                    opacity={menuOpacity}
+                    height={menuHeight}
+                    items={MenuItems}
+                />
             </button>
+
         </nav>
     )
 }
